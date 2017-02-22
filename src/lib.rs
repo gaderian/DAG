97298d14e997bc4f7187d09776bd670273503e4f
erikm <erikm@cs.umu.se>
2017-02-22 16:12:03 +0100

    start of project

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+struct edge<T> {
+    from: u64,
+    to: u64,
+    weight: T,
+}
+
+pub struct DAG<T> {
+    vertices: Vec<T>,
+    edges: Vec<edge<T>>,
+}
+
+impl <T> DAG<T> {
+    fn new() -> DAG<T> {
+        DAG {
+            vertices: Vec::new(),
+            edges: Vec::new()
+        }
+    }
+
+    fn add_vertex(&mut self, w: T) -> u64 {
+        self.vertices.push(w);
+        self.vertices.len() as u64
+    }
+
+    fn add_edge(&mut self, a: u64, b: u64, w: T) {
+        self.edges.push(edge {from: a, to: b, weight: w});
+    }
+}
+        
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_dummy() {
+        assert_eq!(1, 1);
+    }
+}
