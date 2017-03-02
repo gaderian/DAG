698c1d1d9baf9ad9e1ab20a02bc6b69b090ff42a
erikm <erikm@cs.umu.se>
2017-03-02 11:18:49 +0100

    added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 mod interface;
 use interface::DAGInterface;
 
-struct edge<T> {
+#[derive(PartialEq,Debug)]
+struct Edge<T> {
     from: u64,
     to: u64,
     weight: T,
@@ -9,7 +10,7 @@ struct edge<T> {
 
 pub struct DAG<T> {
     vertices: Vec<T>,
-    edges: Vec<edge<T>>,
+    edges: Vec<Edge<T>>,
 }
 
 impl <T> DAG<T> {
@@ -29,16 +30,41 @@ impl <T> DAGInterface<T> for DAG<T> {
     }
 
     fn add_edge(&mut self, a: u64, b: u64, w: T) {
-        self.edges.push(edge {from: a, to: b, weight: w});
+        self.edges.push(Edge {from: a, to: b, weight: w});
     }
 }
         
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
-    fn test_dummy() {
-        assert_eq!(1, 1);
+    fn test_new() {
+        let dag: DAG<u8> = DAG::new();
+        assert!(dag.vertices.is_empty());
+        assert!(dag.edges.is_empty());
+    }
+
+    #[test]
+    fn test_add_vertex() {
+        let mut dag: DAG<u8> = DAG::new();
+        dag.add_vertex(8);
+
+        assert_eq!(1, dag.vertices.len());
+        assert_eq!(Some(8), dag.vertices.pop()); 
+    }
+
+    #[test]
+    fn test_add_edge() {
+        let mut dag: DAG<u8> = DAG::new();
+        let a = dag.add_vertex(5);
+        let b = dag.add_vertex(8);
+
+        dag.add_edge(a, b, 10);
+        assert_eq!(1, dag.edges.len());
+
+        let tmp = Edge { from: a, to: b, weight: 10 };
+        assert_eq!(tmp, dag.edges.pop().unwrap());
     }
 }
