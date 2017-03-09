12e9bb8988d6d95ae3686e04e3b34bf6c82a5723
Jakob Nordström <ens13jnm@cs.umu.se>
2017-03-09 12:58:17 +0100

    added a check to add edge so an edge between a vertex and itself can not be added. Also changed project name in cargo.toml and changed so that the weight of the vertices and edges does not need to have the same tyoes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 mod interface;
 use interface::DAGInterface;
-
 type ID = u64;
 
 #[derive(PartialEq,Debug,Clone)]
@@ -16,14 +15,14 @@ struct Vertex<T> {
     weight: T,
 }
 
-pub struct DAG<T:Clone> {
-    vertices: Vec<Vertex<T>>,
-    edges: Vec<Edge<T>>,
+pub struct DAG<T1:Clone, T2:Clone> {
+    vertices: Vec<Vertex<T1>>,
+    edges: Vec<Edge<T2>>,
     next_id: ID,
 }
 
-impl <T:Clone> DAG<T> {
-    pub fn new() -> DAG<T> {
+impl <T1:Clone, T2:Clone> DAG<T1,T2> {
+    pub fn new() -> DAG<T1, T2> {
         DAG {
             vertices: Vec::new(),
             edges: Vec::new(),
@@ -32,23 +31,32 @@ impl <T:Clone> DAG<T> {
     }
 
     fn topologogal_order(&self) -> Vec<ID> {
-        let mut no_incomming: Vec<Vertex<T>> = Vec::new();
-        let mut remaining_v: Vec<Vertex<T>> = self.vertices[..].to_owned();
-        let mut remaining_e: Vec<Edge<T>> = self.edges[..].to_owned();
+        let mut no_incomming: Vec<Vertex<T1>> = Vec::new();
+        let mut remaining_v: Vec<Vertex<T1>> = self.vertices[..].to_owned();
+        let mut remaining_e: Vec<Edge<T2>> = self.edges[..].to_owned();
         vec![]
     }
 }
 
-impl <T:Clone> DAGInterface<T> for DAG<T> {
+impl <T1:Clone, T2:Clone> DAGInterface<T1,T2> for DAG<T1, T2> {
 
-    fn add_vertex(&mut self, w: T) -> ID {
+    fn add_vertex(&mut self, w: T1) -> ID {
         self.vertices.push(Vertex{id: self.next_id, weight: w});
         self.next_id += 1;
         self.next_id-1
     }
 
-    fn add_edge(&mut self, a: ID, b: ID, w: T) {
+    fn add_edge(&mut self, a: ID, b: ID, w: T2) -> Result<bool, &'static str> {
+        if a == b {
+            return Err("false");
+        }
+
+        for i in 0..self.edges.len() {
+            println!("{:?}", self.edges[i].from);
+        }
+
         self.edges.push(Edge {from: a, to: b, weight: w});
+        Ok(true)
     }
 }
         
@@ -59,14 +67,14 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let dag: DAG<u8> = DAG::new();
+        let dag: DAG<u8,u8> = DAG::new();
         assert!(dag.vertices.is_empty());
         assert!(dag.edges.is_empty());
     }
 
     #[test]
     fn test_add_vertex() {
-        let mut dag: DAG<u8> = DAG::new();
+        let mut dag: DAG<u8,u8> = DAG::new();
         let id = dag.add_vertex(8);
 
         assert_eq!(1, dag.vertices.len());
@@ -75,7 +83,7 @@ mod tests {
 
     #[test]
     fn test_add_edge() {
-        let mut dag: DAG<u8> = DAG::new();
+        let mut dag: DAG<u8,u8> = DAG::new();
         let a = dag.add_vertex(5);
         let b = dag.add_vertex(8);
 
@@ -84,5 +92,17 @@ mod tests {
 
         let tmp = Edge { from: a, to: b, weight: 10 };
         assert_eq!(tmp, dag.edges.pop().unwrap());
+    }
+
+    #[test]
+    fn test_edge_to_self() {
+        let mut dag: DAG<u8,u8> = DAG::new();
+        let a = dag.add_vertex(5);
+
+        //Err is the desired result from add.
+        match dag.add_edge(a,a,19) {
+            Err(_) => assert_eq!(1, 1),
+            Ok(_) => assert_eq!(1, 2)
+        }
     }
 }
